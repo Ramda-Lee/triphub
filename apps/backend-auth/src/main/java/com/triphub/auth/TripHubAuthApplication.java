0b3a80940b6fb0f8e43886bff570eedd6e903116
Ramda-Lee <rkdgml357@naver.com>
2025-04-08 00:20:18 +0900

    chore : TripHub backend 설정

---
+++ apps/backend-auth/src/main/java/com/triphub/auth/TripHubAuthApplication.java
@@ -0,0 +1,13 @@
+package com.triphub.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripHubAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripHubAuthApplication.class, args);
+	}
+
+}
---
+++ apps/backend-auth/src/test/java/com/triphub/auth/TripHubAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triphub.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripHubAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
